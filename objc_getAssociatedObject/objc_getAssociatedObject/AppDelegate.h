5aa827f4d4aa81a4a4bba383594e9637328a0bf0
刘志意 <liuzhiyi@alldk.com>
2017-05-26 14:28:28 +0800

    objc_getAssociatedObject样例1

---
+++ objc_getAssociatedObject/objc_getAssociatedObject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  objc_getAssociatedObject
+//
+//  Created by alldk on 17/5/26.
+//  Copyright © 2017年 alldk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objc_getAssociatedObject/objc_getAssociatedObject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  objc_getAssociatedObject
+//
+//  Created by alldk on 17/5/26.
+//  Copyright © 2017年 alldk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
