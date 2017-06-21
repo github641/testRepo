e50da0e13cbe0dbb470d2ebeb55b00d49358779a
刘志意 <liuzhiyi@alldk.com>
2017-06-21 16:23:33 +0800

    使用Carthage尝试把AFN导入到工程中，熟悉一下这个出了较久但是一直没有尝试的包管理工具。参考的博客在vc中

---
+++ getAFNFromCarthage/getAFNFromCarthage/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  getAFNFromCarthage
+//
+//  Created by alldk on 2017/6/21.
+//  Copyright © 2017年 alldk. All rights reserved.
+/* lzy注170621：
+ http://www.jianshu.com/p/27e9302e56b5
+ http://www.jianshu.com/p/f473c5ca2c8c
+ http://www.jianshu.com/p/42118918177b
+ */
+
+#import "ViewController.h"
+#import <AFNetworking/AFNetworking.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
