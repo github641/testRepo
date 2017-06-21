e4e13efbaa370e95635c9878998434e6f0e807a2
刘志意 <liuzhiyi@alldk.com>
2017-06-21 15:47:16 +0800

    开始自己结合使用经验、文章分析全面回顾下。扩展使用经验，使用的更熟悉，并且把所有的分析文章注释注到源代码中。使用了cocoaPods集成。

---
+++ UseAFN/UseAFN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UseAFN
+//
+//  Created by alldk on 2017/6/21.
+//  Copyright © 2017年 alldk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UseAFN/UseAFN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UseAFN
+//
+//  Created by alldk on 2017/6/21.
+//  Copyright © 2017年 alldk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
