e08f311f9161eb0a46a9c69d79ab78cbf2bce14c
github641 <mrliuzy@outlook.com>
2017-08-28 17:55:46 +0800

    开个学习swift的工程，主要就是看他们翻译的swift3.0的文档。预期目标是把这个中文的文档，敲三遍，然后过下swift4.0的英文文档。然后查一下swift4.0有哪些新的东西。然后找objc中国那基本swift的书看完；然后要么翻译自己做的项目为swift，要么做三个优秀的swift开源项目。这个整个计划要在今年之内弄完，这个文档的事情预期是9月15日之前；4本书计划是3个月弄完，以为coreData那本可以暂时不看，也就到了11月15日；然后是三个开源项目，一定可以在2月份到来之前搞定，干巴爹

---
+++ SwiftProgrammingLanguage/SwiftProgrammingLanguage/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  SwiftProgrammingLanguage
+//
+//  Created by alldk on 2017/8/28.
+//  Copyright © 2017年 alldk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("hello swift")
+        
+        var myVaridalbe = 43
+        
+        myVaridalbe = 55
+        
+        let myConstant = 44
+        
+        
+        
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
