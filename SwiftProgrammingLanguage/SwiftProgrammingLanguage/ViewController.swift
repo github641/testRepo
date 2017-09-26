951e940aeafa791cdd6bfcf9e5cd6e85cf4b4a92
刘志意 <liuzhiyi@alldk.com>
2017-09-26 12:19:03 +0800

    期间找telegram的订阅怎么用去了，花了点儿时间。过了一下下标语法，下一步是『继承』。

--- SwiftProgrammingLanguage/SwiftProgrammingLanguage/ViewController.swift
+++ SwiftProgrammingLanguage/SwiftProgrammingLanguage/ViewController.swift
@@ -2,18 +2,19 @@
 //  ViewController.swift
 //  SwiftProgrammingLanguage
 //
-//  Created by admin on 2017/9/25.
+//  Created by admin on 2017/9/26.
 //  Copyright © 2017年 alldk. All rights reserved.
 //
 /*lzy170920注:
  这个类，对应的是 The Swift Programming Language第二章（Language Guide）的内容：
- 下标
- 1.0 翻译:siemenliu 校对:zq54zquan
- 2.0 翻译+校对:shanks
- 2.1 翻译+校对:shanks，Realank
+ 继承
+ 1.0 翻译:Hawstein 校对:menlongsheng 2.0，2.1 翻译+校对:shanks
  2.2 校对:SketchK 2016-05-13 3.0.1，shanks，2016-11-13
  本页包含内容:
- • 下标语法 (页 0) • 下标用法 (页 0) • 下标选项 (页 0)
+ • 定义一个基类 (页 0)
+ • 子类生成 (页 0)
+ • 重写 (页 0)
+ • 防止重写 (页 0)
  */
 import UIKit
 
@@ -21,106 +22,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-         /*下标可以定义在类、结构体和枚举中，是访问 合，列表或序列中元素的快捷方式。可以使用下标的索引，设置 和获取值，而不需要再调用对应的存取方法。举例来说，用下标访问一个 Array 实例中的元素可以写作 y[index] ，访问 Dictionary 实例中的元素可以写作 someDictionary[key] 。
-         一个类型可以定义多个下标，通过不同索引类型进行重载。下标不限于一维，你可以定义具有多个入参的下标满
-         足自定义类型的需求。
-         下标语法
-         下标允许你通过在实例名称后面的方括号中传入一个或者多个索引值来对实例进行存取。语法类似于实例方法语 法和计算型属性语法的混合。与定义实例方法类似，定义下标使用 subscript 关键字，指定一个或多个输入参数 和返回类型;与实例方法不同的是，下标可以设定为读写或只读。这种行为由 getter 和 setter 实现，有点类 似计算型属性:
-         someArra
-         subscript(index: Int) -> Int {
-         get {
-         // 返回一个适当的 Int 类型的值 }
-         set(newValue) {
-         // 执行适当的赋值操作
-         } }
-         第 2 章 Swift 教程 | 173
-         newValue 的类型和下标的返回类型相同。如同计算型属性，可以不指定 setter 的参数( newValue )。如果不 指定参数，setter 会提供一个名为 newValue 的默认参数。
-         如同只读计算型属性，可以省略只读下标的 get 关键字:
-         subscript(index: Int) -> Int {
-         // 返回一个适当的 Int 类型的值
-         }
-         下面代码演示了只读下标的实现，这里定义了一个 TimesTable 结构体，用来表示传入整数的乘法表:
-         struct TimesTable {
-         let multiplier: Int
-         subscript(index: Int) -> Int {
-         return multiplier * index
-         }
-         }
-         let threeTimesTable = TimesTable(multiplier: 3) print("six times three is \(threeTimesTable[6])") // 打印 "six times three is 18"
-         在上例中，创建了一个 TimesTable 实例，用来表示整数 3 的乘法表。数值 3 被传递给结构体的构造函数，作为 实例成员 multiplier 的值。
-         你可以通过下标访问 threeTimesTable 实例，例如上面演示的 threeTimesTable[6] 。这条语句查询了 3 的乘法表 中的第六个元素，返回3的6倍即18。
-         注意
-         TimesTable 例子基于一个固定的数学公式，对 threeTimesTable[someIndex] 进行赋值操作并不合适，因此下标
-         定义为只读的。
-         下标用法
-         下标的确切含义取决于使用场景。下标通常作为访问 合，列表或序列中元素的快捷方式。你可以针对自己特定
-         的类或结构体的功能来自由地以最恰当的方式实现下标。
-         例如，Swift 的 Dictionary 类型实现下标用于对其实例中储存的值进行存取操作。为字典设值时，在下标中使用 和字典的键类型相同的键，并把一个和字典的值类型相同的值赋给这个下标:
-         var numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
-         numberOfLegs["bird"] = 2
-         上例定义一个名为 numberOfLegs 的变量，并用一个包含三对键值的字典字面量初始化它。 numberOfLegs 字典的 类型被推断为 [String: Int] 。字典创建完成后，该例子通过下标将 String 类型的键 bird 和 Int 类型的值 2 添 加到字典中。
-         
-         第 2 章 Swift 教程 | 174
-         更多关于 Dictionary 下标的信息请参考读取和修改字典 (页 0)
-         注意
-         Swift 的 Dictionary 类型的下标接受并返回可选类型的值。上例中的 numberOfLegs 字典通过下标返回的是一个
-         Int? 或者说“可选的int”。 Dictionary 类型之所以如此实现下标，是因为不是每个键都有个对应的值，同时 这也提供了一种通过键删除对应值的方式，只需将键对应的值赋值为 nil 即可。
-         下标选项
-         下标可以接受任意数量的入参，并且这些入参可以是任意类型。下标的返回值也可以是任意类型。下标可以使用
-         变量参数和可变参数，但不能使用输入输出参数，也不能给参数设置默认值。
-         一个类或结构体可以根据自身需要提供多个下标实现，使用下标时将通过入参的数量和类型进行区分，自动匹配
-         合适的下标，这就是下标的重载。
-         虽然接受单一入参的下标是最常见的，但也可以根据情况定义接受多个入参的下标。例如下例定义了一个 结构体，用于表示一个 Double 类型的二维矩阵。 Matrix 结构体的下标接受两个整型参数:
-         Matrix
-         struct Matrix {
-         let rows: Int, columns: Int
-         var grid: [Double]
-         init(rows: Int, columns: Int) {
-         self.rows = rows
-         self.columns = columns
-         grid = Array(count: rows * columns, repeatedValue: 0.0)
-         }
-         func indexIsValidForRow(row: Int, column: Int) -> Bool {
-         return row >= 0 && row < rows && column >= 0 && column < columns
-         }
-         subscript(row: Int, column: Int) -> Double {
-         get {
-         assert(indexIsValidForRow(row, column: column), "Index out of range")
-         return grid[(row * columns) + column]
-         }
-         set {
-         assert(indexIsValidForRow(row, column: column), "Index out of range")
-         grid[(row * columns) + column] = newValue
-         } }
-         }
-         Matrix 提供了一个接受两个入参的构造方法，入参分别是 rows 和 columns ，创建了一个足够容纳
-         ns 个 Double 类型的值的数组。通过传入数组长度和初始值 0.0 到数组的构造器，将矩阵中每个位置的值初始化 为 0.0 。关于数组的这种构造方法请参考创建一个空数组 (页 0)。
-         你可以通过传入合适的 row 和 column 的数量来构造一个新的 Matrix 实例:
-         rows * colum
-         var matrix = Matrix(rows: 2, columns: 2)
-         第 2 章 Swift 教程 | 175
-         上例中创建了一个 Matrix 实例来表示两行两列的矩阵。该 Matrix 实例的 grid 数组按照从左上到右下的阅读顺 序将矩阵扁平化存储:
-         将 row 和 column 的值传入下标来为矩阵设值，下标的入参使用逗号分隔:
-         matrix[0, 1] = 1.5
-         matrix[1, 0] = 3.2
-         上面两条语句分别调用下标的 setter 将矩阵右上角位置(即 row 为 0 、 column 为 1 的位置)的值设置为 1.5 ，将矩阵左下角位置(即 row 为 1 、 column 为 0 的位置)的值设置为 3.2 :
-         Matrix 下标的 getter 和 setter 中都含有断言，用来检查下标入参 row 和 column 的值是否有效。为了方便进 行断言， Matrix 包含了一个名为 indexIsValidForRow(_:column:) 的便利方法，用来检查入参 row 和 column 的值 是否在矩阵范围内:
-         func indexIsValidForRow(row: Int, column: Int) -> Bool {
-         return row >= 0 && row < rows && column >= 0 && column < columns
-         } 断言在下标越界时触发:
-         let someValue = matrix[2, 2]
-         // 断言将会触发，因为 [2, 2] 已经超过了 matrix 的范围
-         第 2 章 Swift 教程 | 176
-         继承
-         1.0 翻译:Hawstein 校对:menlongsheng 2.0，2.1 翻译+校对:shanks
-         2.2 校对:SketchK 2016-05-13 3.0.1，shanks，2016-11-13
-         本页包含内容:
-         • 定义一个基类 (页 0) • 子类生成 (页 0)
-         • 重写 (页 0)
-         • 防止重写 (页 0)
+
+        /*lzy170926注:
          一个类可以继承另一个类的方法，属性和其它特性。当一个类继承其它类时，继承类叫子类，被继承类叫超 类(或父类)。在 Swift 中，继承是区分「类」与其它类型的一个基本特征。
-         在 Swift 中，类可以调用和访问超类的方法，属性和下标，并且可以重写这些方法，属性和下标来优化或修改它 们的行为。Swift 会检查你的重写定义在超类中是否有匹配的定义，以此确保你的重写行为是正确的。
+         在 Swift 中，类可以调用和访问超类的方法，属性和下标，并且可以重写这些方法，属性和下标来优化或修改它们的行为。Swift 会检查你的重写定义在超类中是否有匹配的定义，以此确保你的重写行为是正确的。
          可以为类中继承来的属性添加属性观察器，这样一来，当属性值改变时，类就会被通知到。可以为任何属性添加
          属性观察器，无论它原本被定义为存储型属性还是计算型属性。
          定义一个基类 不继承于其它类的类，称之为基类。
@@ -280,13 +185,7 @@ class ViewController: UIViewController {
          你可以通过把方法，属性或下标标记为   来防止它们被重写，只需要在声明关键字前加上   修饰符即 可(例如: ， ， ，以及 )。
          如果你重写了带有   标记的方法，属性或下标，在编译时会报错。在类扩展中的方法，属性或下标也可以在 扩展的定义里标记为 final 的。
          你可以通过在关键字   前添加   修饰符(   )来将整个类标记为 final 的。这样的类是不可 被继承的，试图继承这样的类会导致编译报错。
-         的   属性，属性的   观察器就会自动地设置   属性，为新的速度选
-         
          */
-         
-        
-        
     }
-
 
 }
